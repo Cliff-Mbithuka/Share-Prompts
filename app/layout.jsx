f50e1951047ec9aa3d85e82337b4a4d2afee391e
Clifford Musyoka <cliffordmusyoka1@gmail.com>
2025-01-28 10:59:44 +0300

    Add all components

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import '@styles/globals.css';
+
+
+export const metadata = {
+    title: "Promptopia",
+    description: 'Discover & share AI Prompts'
+}
+
+const RootLayout = ({children}) => {
+  return (
+   <html lang='en'>
+    <body>
+        <div className="main">
+            <div className="gradient"/>
+        </div>
+
+        <div className="app">
+            {children}
+        </div>
+    </body>
+   </html>
+  )
+}
+
+export default RootLayout
