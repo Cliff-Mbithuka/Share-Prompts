fd24e05db53c76790dc3bc7e01928ed7d1ddd2f9
Clifford Musyoka <cliffordmusyoka1@gmail.com>
2025-01-28 15:19:26 +0300

    Style the nav bar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import '@styles/globals.css';
 
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
 
 export const metadata = {
     title: "Promptopia",
@@ -15,6 +17,7 @@ const RootLayout = ({children}) => {
         </div>
 
         <div className="app">
+            <Nav/>
             {children}
         </div>
     </body>
